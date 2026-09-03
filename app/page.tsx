444920fa7562c7c92a4b55278c0d4ed5772b3d0c
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2026-09-02 21:03:01 -0700

    Use progressive recognition media in the main lesson

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import "./styles/v02.css";
+import "./styles/media.css";
 
+import { TraceMediaShell } from "@/components/v02/TraceMediaShell";
 import { TraceTrainer } from "@/components/v02/TraceTrainer";
 import { greenPepperStory } from "@/data/stories";
 
 export default function HomePage() {
-  return <TraceTrainer story={greenPepperStory} />;
+  return (
+    <TraceMediaShell story={greenPepperStory}>
+      <TraceTrainer story={greenPepperStory} />
+    </TraceMediaShell>
+  );
 }
