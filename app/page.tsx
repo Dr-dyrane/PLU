b60604ebf404932f1f3946767fbd3a13d0188ea4
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2026-09-02 21:57:47 -0700

    Use the tested canonical PLU lesson

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import "./styles/v02.css";
-import "./styles/media.css";
+import "./styles/canon.css";
 
-import { TraceMediaShell } from "@/components/v02/TraceMediaShell";
-import { TraceTrainer } from "@/components/v02/TraceTrainer";
+import { PluLesson } from "@/components/canon/PluLesson";
 import { greenPepperStory } from "@/data/stories";
 
 export default function HomePage() {
-  return (
-    <TraceMediaShell story={greenPepperStory}>
-      <TraceTrainer story={greenPepperStory} />
-    </TraceMediaShell>
-  );
+  return <PluLesson story={greenPepperStory} />;
 }
