144c58fb2f36dda55c9bf7639a1d4ae9609a1fdb
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2026-09-03 12:01:54 -0700

    Use lightweight home-card data on Batch 03

--- app/batch-03/page.tsx
+++ app/batch-03/page.tsx
@@ -1,12 +1,12 @@
 import { BatchHome } from "@/components/canon/BatchHome";
 import { HomeFooter } from "@/components/canon/HomeFooter";
 import { batch03 } from "@/data/batches";
-import { productStories } from "@/data/stories";
+import { homeStorySummaries } from "@/data/stories";
 
 export default function Batch03Page() {
   return (
     <>
-      <BatchHome batch={batch03} stories={productStories} />
+      <BatchHome batch={batch03} stories={homeStorySummaries} />
       <HomeFooter />
     </>
   );
